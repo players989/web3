aa6e417223d6603a915dfd31ad9e4ff856abfac8
lkj <1192114937@qq.com>
2023-05-05 09:45:47 +0800

    '11111'

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("this my app.js")
+//初始化 git init
+//查看文件状态 git status
+// 把文件从工作区提交到暂存区 用git add 文件名 或者 git add. (这是把全部文件提交）
+//把文件从暂存区提交到git本地仓库 ；git commit -m'提交信息
+// git log  查看提交记录
+//撤销 从暂存区 回退到工作区 git  check back
+// git rm --cached 把文件从暂存区中撤销
+//git 分支
+// Git的三个状态 已提交（committed)已修改(modified) 已缓存(staged)
+// 三个区 工作区 暂存区 本地GIT仓库
+//git branch 查看分支
+//git branch 分支名称 创建分支
+//git checkout 分支名称 切换分支
+//git branch -d 分支名称 删除分支
+// git merge 分支名称 合并分支
+//git stash 存储临时分支改动
+//恢复改动 git stash  pop
+//创立远程仓库 git
+
+// git remote add 别名 远程git仓库地址
+// 提交 git push -u 别名 分支名
+
+
+
+//使用ssh 登录使用
+//创建公共key ；ssh-keygen
+
+
+
+
